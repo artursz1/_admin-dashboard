b80a141c3e0e0a2ae0cc53472f43a39592ca9587
artursz1 <arturszabo7@gmail.com>
2023-02-06 00:57:20 +0200

    added .env file and loading credentials from there

--- src/data/supabaseClient.js
+++ src/data/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://slggthbwaspmopioojts.supabase.co';
-const serviceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZ2d0aGJ3YXNwbW9waW9vanRzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NTQyNTE3MCwiZXhwIjoxOTkxMDAxMTcwfQ.9FV3qe6VIr46KFydmZsb8-yVoq_y-bzaPetrDUSHJ10';
+const supabaseUrl = process.env.SUPABASE_URL;
+const serviceKey = process.env.SUPABASE_SERVICE_KEY;
 
 export default createClient(supabaseUrl, serviceKey);
